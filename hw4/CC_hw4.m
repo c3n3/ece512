1c25aaedcd7a46f456ff17fcf3f73eb48f140cf9
Caden Churchman <ccaden21@gmail.com>
2021-10-08 19:28:02 -0500

    Started the matlab

---
+++ hw4/CC_hw4.m
@@ -0,0 +1,13 @@
+%---------------------------------------------------------------------
+% File: CC_hw4.m
+% Description: This script plots various circuit functions.
+% Author: Caden Churchman, ccaden21@ksu.edu,
+% (c)2021, Caden Churchman. All rights reserved.
+% Date: 10 September 2021(Version 1.00)
+% Platform: MATLAB Release R2021a, Ubuntu 20.04
+% Reference: Steve Warren. ECE 512 Homework Assignment #4, (c)2021,
+% Kansas State University
+%---------------------------------------------------------------------
+
+
+% We do convolutions
---
+++ hw4/physConv_CC.m
@@ -0,0 +1,4 @@
+function [y] = physConv_CC(x,h,dTau)
+    y = zeros(10);
+end
+
